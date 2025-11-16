034af83cd6b68e39cf7b36ea712139dd17ca6247
Bennimin <benjaminwong8008@gmail.com>
2025-11-16 13:06:43 -0800

    a
    s

---
+++ PROJECT_2 R/Aim 1 alpha diversity.R
@@ -0,0 +1,58 @@
+library(phyloseq)
+library(tidyverse)
+library(RColorBrewer)
+
+load(file = "phylseq_files.RData")
+
+### Plotting plco observed and shannon alpha diversity ###
+rich_plot_plco <- plot_richness(plco_rare, x="disease_alc_status", measures=c("Observed", "Shannon")) +
+  geom_boxplot(aes(fill = disease_alc_status)) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(
+    x = "Cancer and Alcohol Status",
+    y = "Alpha Diversity Measure",
+    fill = "Cancer and Alcohol Status") +
+  scale_x_discrete(labels = label) +
+  scale_fill_discrete(labels = label) +
+  scale_fill_manual(values = palette, labels = label)
+rich_plot_plco
+
+### Kruskal wallis test on plco alpha diversity ###
+alphadiv_plco <- estimate_richness(plco_rare, measures = c("Observed", "Shannon")) |>
+  rownames_to_column(var = "sample.id") |>
+  left_join(as.data.frame(sample_data(plco_rare)), by = "sample.id")
+head(alphadiv_plco)
+kruskal.test(Shannon ~ disease_alc_status, data = alphadiv_plco)
+
+
+### Plotting  cpsII observed and shannon slpah diveresity ###
+rich_plot_cpsII <- plot_richness(cpsII_rare, x="disease_alc_status", measures=c("Observed", "Shannon")) +
+  geom_boxplot(aes(fill = disease_alc_status)) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(
+    x = "Cancer and Alcohol Status",
+    y = "Alpha Diversity Measure",
+    fill = "Cancer and Alcohol Status") +
+  scale_x_discrete(labels = label) +
+  scale_fill_discrete(labels = label) +
+  scale_fill_manual(values = palette, labels = label)
+rich_plot_cpsII
+
+
+### Kruskal wallis test on cpsII alpha diversity ###
+alphadiv_cpsII <- estimate_richness(cpsII_rare, measures = c("Observed", "Shannon")) |>
+  rownames_to_column(var = "sample.id") |>
+  left_join(as.data.frame(sample_data(cpsII_rare)), by = "sample.id")
+head(alphadiv_cpsII)
+kruskal.test(Shannon ~ disease_alc_status, data = alphadiv_cpsII)
+
+
+ggsave("rp_plco.png"
+       , rich_plot_plco
+       , height = 5, width = 10)
+
+ggsave("rp_cpsII.png"
+       , rich_plot_cpsII
+       , height = 5, width = 10)
