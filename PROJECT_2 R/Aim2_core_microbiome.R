d86865dac0f906ddaa2fa3eb2122aa51a5ea589a
alim057 <44333439+alim057@users.noreply.github.com>
2025-11-28 22:10:53 -0800

    changed color theme

--- PROJECT_2 R/Aim2_core_microbiome.R
+++ PROJECT_2 R/Aim2_core_microbiome.R
@@ -38,47 +38,15 @@ core_microbiome_plot <- ggVennDiagram(x=list(
   "No Cancer, Yes" = core_control_yes_alc,
   "No Cancer, No" = core_control_no_alc
 ),label_alpha = 0, set_size=4) +
-  ggplot2::scale_fill_distiller(direction = 1) +
   theme_void() +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
   scale_x_continuous(expand = expansion(mult = 0.2)) +
-  labs(title = "Core Microbiome Venn Diagram (prevalence > 40% & abundance > 0.01%)")
+  labs(title = "prevalence > 40% & abundance > 0.01%") +
+  scale_fill_gradient(low = "#F4FAFE", high = "#4981BF")
 core_microbiome_plot
-
-# 1. Agglomerate at genus level
-cpsII_genus_unrare <- tax_glom(CPSII, taxrank = "Genus", NArm = FALSE)
-
-# transform data to relative abundance
-cpsII_rel <- transform_sample_counts(cpsII_genus_unrare, fun = function(x) x/sum(x))
-
-# subset dataset into 4 groups from testing data
-test_cancer_yes <- subset_samples(cpsII_rel, disease_alc_status == "Case:yes")
-test_cancer_no <- subset_samples(cpsII_rel, disease_alc_status == "Case:no")
-test_control_yes <- subset_samples(cpsII_rel, disease_alc_status == "Control:yes")
-test_control_no <- subset_samples(cpsII_rel, disease_alc_status == "Control:no")
-
-#detect the core microbiome for each group
-test_core_cancer_yes <- core_members(test_cancer_yes, detection = abund_threshold, prevalence = prev_threshold)
-test_core_cancer_no <- core_members(test_cancer_no, detection = abund_threshold, prevalence = prev_threshold)
-test_core_control_yes <- core_members(test_control_yes, detection = abund_threshold, prevalence = prev_threshold)
-test_core_control_no <- core_members(test_control_no, detection = abund_threshold, prevalence = prev_threshold)
-
-# plot the core microbiome
-test_microbiome_plot <- ggVennDiagram(x=list(
-  "Cancer, Yes" = test_core_cancer_yes,
-  "Cancer, No" = test_core_cancer_no,
-  "No Cancer, Yes" = test_core_control_yes,
-  "No Cancer, No" = test_core_control_no
-),label_alpha = 0, set_size=4) +
-  ggplot2::scale_fill_distiller(direction = 1) +
-  theme_void() +
-  scale_x_continuous(expand = expansion(mult = 0.2)) +
-  labs(title = "Core Microbiome Venn Diagram (prevalence > 40% & abundance > 0.01%)")
-test_microbiome_plot
 
 # save plots
 ggsave("microbiome_plco.png",
        core_microbiome_plot,
-       height = 4, width = 6)
-ggsave("microbiome_cpsII.png",
-       test_microbiome_plot,
-       height = 4, width = 6)
+       height = 5, width = 7)
+
