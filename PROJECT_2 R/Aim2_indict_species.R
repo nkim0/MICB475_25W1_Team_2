c1df544eea2ac4f6794d68519856e48c137541b1
alim057 <44333439+alim057@users.noreply.github.com>
2025-11-28 23:24:50 -0800

    Removed figure and created new comparison table

--- PROJECT_2 R/Aim2_indict_species.R
+++ PROJECT_2 R/Aim2_indict_species.R
@@ -1,6 +1,7 @@
 library(phyloseq)
 library(indicspecies)
 library(tidyverse)
+library(kableExtra)
 
 # load objects
 load(file = "phylseq_files.RData")
@@ -8,85 +9,53 @@ load(file = "phylseq_files.RData")
 ### AIM 2.2: Indicator Species Analysis ###
 # Agglomerate at genus level and convert to relative abundance
 PLCO_genus <- tax_glom(PLCO, taxrank = "Genus", NArm = FALSE)
-PLCO_genus_RA <- transform_sample_counts(PLCO_genus, fun=function(x) x/sum(x))
-
-CPSII_genus <- tax_glom(CPSII, taxrank = "Genus", NArm = FALSE)
-CPSII_genus_RA <- transform_sample_counts(CPSII_genus, fun=function(x) x/sum(x))
+PLCO_genus_RA <- transform_sample_counts(PLCO_genus, fun = function(x) x / sum(x))
 
 # Run indicator species analysis using the donor_status variable and look at results
-isa_plco <- multipatt(t(otu_table(PLCO_genus_RA)), cluster = sample_data(PLCO_genus_RA)$'disease_alc_status')
+isa_plco <- multipatt(t(otu_table(PLCO_genus_RA)), cluster = sample_data(PLCO_genus_RA)$"disease_alc_status")
 summary(isa_plco)
 
-isa_cpsII <- multipatt(t(otu_table(CPSII_genus_RA)), cluster = sample_data(CPSII_genus_RA)$'disease_alc_status')
-summary(isa_cpsII)
-
 # Extract taxonomy table
-plco_taxtable <- tax_table(plco_rare) |> as.data.frame() |> rownames_to_column(var="ASV")
-
-cpsII_taxtable <- tax_table(cpsII_rare) |> as.data.frame() |> rownames_to_column(var="ASV")
+plco_taxtable <- tax_table(PLCO) |>
+  as.data.frame() |>
+  rownames_to_column(var = "ASV")
 
 # Merge taxonomy table with phyloseq object and filter by significant p-value
 isa_res <- isa_plco$sign |>
-  rownames_to_column(var="ASV") |>
+  rownames_to_column(var = "ASV") |>
   left_join(plco_taxtable) |>
   filter(p.value < 0.05)
 view(isa_res)
 
-isa_cpsII_res <- isa_cpsII$sign |>
-  rownames_to_column(var="ASV") |>
-  left_join(cpsII_taxtable) |>
-  filter(p.value < 0.05)
-view(isa_cpsII_res)
+# Format ISA results table
+isa_plco_table <- isa_res |>
+  filter(!is.na(Genus)) |>
+  mutate(Genus = str_remove(Genus, "^g__")) |>
+  mutate(Genus = str_replace_all(Genus, "_", " ")) |>
+  select(Genus, 
+    "Cancer, No"     = `s.Case:no`,
+    "Cancer, Yes"    = `s.Case:yes`,
+    "No Cancer, No"  = `s.Control:no`, 
+    "No Cancer, Yes" = `s.Control:yes`, 
+    p.value, stat)|>
+  mutate(stat = round(stat, 3)) |>
+  arrange(p.value) |>
+  rename("Indicator Value" = stat,
+         "P-value" = p.value)
 
+# Create formatted table for PLCO
+kable_plco <- isa_plco_table |>
+  mutate(across(c("Cancer, No", "Cancer, Yes", "No Cancer, No", "No Cancer, Yes"), 
+                ~ ifelse(. == 1, "✔", ""))) |>
+  kbl(align = c("l", "c", "c", "c", "c", "c", "c", "c")) |>
+  kable_styling(bootstrap_options = c("striped", "hover"),
+                full_width = FALSE,
+                font_size = 12) |>
+  add_header_above(c(" " = 1, "Cancer Status and Alcohol Consumption" = 4, " " = 2)) |>
+  column_spec(1, bold = TRUE, italic = TRUE) |>
+  row_spec(0, bold = TRUE, background = "#E8E8E8")
 
-# melt PLCO data and filter for significant genus
-melted_plco <- psmelt(PLCO_genus_RA) |>
-  filter(Genus %in% isa_res$Genus) |>
-  filter(!str_detect(Genus, "^NA"))
+kable_plco
 
-# plot the graph facetted with Genus and relative abundance of each sample in each category
-ISA_plco_plot <- ggplot(melted_plco, aes(x = disease_alc_status, y = Abundance)) +
-  geom_boxplot(aes(fill = disease_alc_status), outlier.shape = NA) +
-  geom_jitter(width = 0.1, alpha = 0.5) +
-  facet_wrap(~ Genus, scales = "free_y") +
-  labs(
-    x = "Cancer and Alcohol Status",
-    y = "Relative Abundance",
-    fill = "Cancer and Alcohol Status") +
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        strip.background = element_blank()) +
-  scale_fill_manual(values = palette, labels = label)
-ISA_plco_plot
-
-
-# melt CPSII data and filter for significant genus
-melted_cpsII <- psmelt(CPSII_genus_RA) |>
-  filter(Genus %in% isa_cpsII_res$Genus) |>
-  filter(!str_detect(Genus, "^NA"))
-
-# plot the graph facetted with Genus and relative abundance of each sample in each category
-ISA_cpsII_plot <- ggplot(melted_cpsII, aes(x = disease_alc_status, y = Abundance)) +
-  geom_boxplot(aes(fill = disease_alc_status), outlier.shape = NA) +
-  geom_jitter(width = 0.1, alpha = 0.5) +
-  facet_wrap(~ Genus, scales = "free_y") +
-  labs(
-    x = "Cancer and Alcohol Consumption",
-    y = "Relative Abundance",
-    fill = "Cancer and Alcohol Consumption") +
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        strip.background = element_blank()) +
-  scale_fill_manual(values = palette, labels = label)
-ISA_cpsII_plot
-
-# save plots
-ggsave("ISA_PLCO.png",
-       ISA_plco_plot,
-       height = 4, width = 6)
-ggsave("ISA_CPSII.png",
-       ISA_cpsII_plot,
-       height = 4, width = 6)
-
-# save isa results
-save(isa_res,isa_cpsII_res, file="ISA.RData")
+# Save as HTML
+save_kable(kable_plco, file = "PLCO_ISA_table.html")
